6e7734149ff5f92be6d9b1627c7eb56420b9ec61
salamaaa <mohamed.salamafciscu@gmail.com>
2025-04-11 18:39:07 +0200

    adding the project structure

---
+++ SecurityEx/src/test/java/com/softpedia/SecurityEx/SecurityExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softpedia.SecurityEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
