b14444e06edd6434ea0ecac46b39d9fd5d0d2b5a
salamaaa <mohamed.salamafciscu@gmail.com>
2025-04-13 19:42:33 +0200

    Adding custom AuthenticationProvider with passwordEncoder and UserDetilsService

--- SecurityEx/src/main/java/com/softpedia/SecurityEx/config/SecurityConfig.java
+++ SecurityEx/src/main/java/com/softpedia/SecurityEx/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.softpedia.SecurityEx.config;
 import jdk.jfr.Frequency;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,12 +12,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -31,22 +40,31 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails mohamed = User
-                .withDefaultPasswordEncoder()
-                .username("mohamed")
-                .password("123*654")
-                .roles("ADMIN")
-                .build();
-
-        UserDetails mona = User
-                .withDefaultPasswordEncoder()
-                .username("mona")
-                .password("123*654")
-                .roles("EMP")
-                .build();
-
-        return new InMemoryUserDetailsManager(mohamed,mona);
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());   //plain text not encoded
+        provider.setUserDetailsService(userDetailsService);                //set User
+        return provider;
     }
+
+    //@Bean
+    //public UserDetailsService userDetailsService(){
+
+//        UserDetails mohamed = User
+//                .withDefaultPasswordEncoder()
+//                .username("mohamed")
+//                .password("123*654")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails mona = User
+//                .withDefaultPasswordEncoder()
+//                .username("mona")
+//                .password("123*654")
+//                .roles("EMP")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(mohamed,mona);
+    //}
 
 }
